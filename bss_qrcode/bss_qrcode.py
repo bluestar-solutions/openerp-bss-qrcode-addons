1eb2d741dd32efc0da33b4c06c054cd5430b7a5e
Mourad Bengmah <mourad.bengmah@blues2.ch>
2014-03-20 16:21:28 +0100

    qr

--- bss_qrcode/bss_qrcode.py
+++ bss_qrcode/bss_qrcode.py
@@ -36,7 +36,7 @@ class bss_qrcode(osv.osv):
         # QR Code creation
         qr = qrcode.QRCode(
             version = None,
-            error_correction = qrcode.constants.ERROR_CORRECT_M,
+            error_correction = qrcode.constants.ERROR_CORRECT_L,
             box_size = size,
             border = 1,
         )
