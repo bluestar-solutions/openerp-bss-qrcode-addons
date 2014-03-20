f09f6cc00e1de3781fc5347c62bfcd089eeb56d0
Mourad Bengmah <mourad.bengmah@blues2.ch>
2014-03-20 11:20:06 +0100

    Size

--- bss_qrcode/bss_qrcode.py
+++ bss_qrcode/bss_qrcode.py
@@ -36,7 +36,7 @@ class bss_qrcode(osv.osv):
         qr = qrcode.QRCode(
             version = 1,
             error_correction = qrcode.constants.ERROR_CORRECT_M,
-            box_size = 3,
+            box_size = 10,
             border = 0,
         )
 
