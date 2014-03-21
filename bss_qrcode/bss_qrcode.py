b60bb23a6591b086431e83aa3912763fa9c4a6ae
Mourad Bengmah <mourad.bengmah@blues2.ch>
2014-03-21 11:42:03 +0100

    qr

--- bss_qrcode/bss_qrcode.py
+++ bss_qrcode/bss_qrcode.py
@@ -31,13 +31,12 @@ class bss_qrcode(osv.osv):
     _name = 'bss_qrcode.qrcode'
     _description = "QR Code generation and files association"
     
-    def print_qrcode(self, cr, uid, ids, size, version, context, report, filename, server_id, specific={}):
+    def print_qrcode(self, cr, uid, ids, version, context, report, filename, server_id, specific={}):
         
         # QR Code creation
         qr = qrcode.QRCode(
             version = None,
             error_correction = qrcode.constants.ERROR_CORRECT_L,
-#            box_size = size,
             border = 1,
         )
         
