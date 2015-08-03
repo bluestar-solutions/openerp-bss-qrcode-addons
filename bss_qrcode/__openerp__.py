56fc61309e97889f03b2bfd9d307342b9ef3d0c3
Daniel Le Gall <daniel.legall@blues2.ch>
2015-08-03 11:44:10 +0200

    fix version build

--- bss_qrcode/__openerp__.py
+++ bss_qrcode/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Bluestar Solutions Sàrl QRCode',
-    'version': '7.0.1.0-20150409',
+    'version': '7.0.1.0-20150803',
     "category": 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """QR Code management for reports.""",
